687366a4a007d4939d780b0f336e3d01c01ee082
Juned Khan <junedkhanc101@gmail.com>
2024-07-13 20:47:51 +0530

    added dark mode styles for help section and feedback section

--- content.js
+++ content.js
@@ -1,26 +1,28 @@
 function applyDarkMode(darkModeEnabled) {
     var appRoot = document.getElementById("appRoot");
-    if (!appRoot) {
-        console.error("No app root found");
-        return;
-    }
-    var root = appRoot.querySelector('div[data-automationid="main-app-container"]');
-    if (!root) {
-        console.error("App container not found");
-        return;
-    }
+    var html = document.querySelector("html");
+    var body = document.querySelector("body");
     if (darkModeEnabled) {
-        root.setAttribute("data-theme", "dark");
-        appRoot.setAttribute("data-theme", "dark");
-        applyStylesToElement(root, "dark");
+        if (appRoot) {
+            var root = appRoot.querySelector('div[data-automationid="main-app-container"]');
+            root.setAttribute("data-theme", "dark");
+            appRoot.setAttribute("data-theme", "dark");
+            applyStylesToElement(root, "dark");
+        }
+        html.setAttribute("data-theme", "dark");
+        body.setAttribute("data-theme", "dark");
     } else {
-        root.setAttribute("data-theme", "light");
-        appRoot.setAttribute("data-theme", "light");
-        applyStylesToElement(root, "light");
+        if (appRoot) {
+            var root = appRoot.querySelector('div[data-automationid="main-app-container"]');
+            root.setAttribute("data-theme", "light");
+            appRoot.setAttribute("data-theme", "light");
+            applyStylesToElement(root, "light");
+        }
+        html.setAttribute("data-theme", "light");
+        body.setAttribute("data-theme", "light");
     }
     chrome.storage.local.set({ darkModeStatus: darkModeEnabled });
 }
-
 function applyStylesToElement(root, theme) {
     root.classList.remove("dark-theme", "light-theme");
     if (theme === "dark") {
